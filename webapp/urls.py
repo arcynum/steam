c48912ece012371009c92eb8e38c8d5f09793d6f
Chris Hamilton <arcynum@gmail.com>
2015-11-21 22:27:56 +0000

    Added some more templates and made some changes to the views file.

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
+	url(r'^$', views.index),
+	url(r'^contacts/$', views.contact_index),
+	url(r'^contact/(?P<contact_id>[0-9]+)/$', views.contact_view, name='contact_view'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import get_object_or_404, render
-from webapp.models import WorkOrder, PurchaseOrder
+from django.http import Http404
+
+from webapp.models import Contact, WorkOrder, PurchaseOrder
 
 def index(request):
 	# return render(request, 'home.html', {})
@@ -10,3 +12,14 @@ def index(request):
 		'work_orders': work_orders,
 		'purchase_orders': purchase_orders
 	})
+
+def contact_index(request):
+	contacts = Contact.objects.order_by('modified')[:5]
+	return render(request, 'contacts/index.html', {'contacts': contacts})
+
+def contact_view(request, contact_id):
+	try:
+		contact = Contact.objects.get(pk=contact_id)
+	except Contact.DoesNotExist:
+		raise Http404("Contact does not exist")
+	return render(request, 'contacts/view.html', {'contact': contact})
